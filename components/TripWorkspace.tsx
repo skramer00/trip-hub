3334501a123a1f31762856cdf43d8a9e8d1929f0
Scott Kramer <scott.skweb@gmail.com>
2026-08-26 11:20:57 -0700

    Use shared active-trip storage key

--- components/TripWorkspace.tsx
+++ components/TripWorkspace.tsx
@@ -3,11 +3,10 @@
 import {useEffect} from 'react';
 import TripApp from '@/components/TripApp';
 import {DEFAULT_TRIP_ID,normalizeTripId} from '@/lib/trips';
-
-export const activeTripStorageKey='trip-active-id';
+import {activeTripStorageKey} from '@/lib/active-trip';
 
 export default function TripWorkspace({tripId=DEFAULT_TRIP_ID}:{tripId?:string}){
  const id=normalizeTripId(tripId);
- useEffect(()=>{sessionStorage.setItem(activeTripStorageKey,id);return()=>{};},[id]);
+ useEffect(()=>{sessionStorage.setItem(activeTripStorageKey,id);},[id]);
  return <TripApp/>;
 }
